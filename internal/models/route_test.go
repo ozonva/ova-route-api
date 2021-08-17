fd8048a64207bbb7e09f3b16cd17b5d443823997
Koba <dkoba@pikemedia.ru>
2021-08-17 23:49:08 +0300

    task-3: Реализовал чтение конфига в фоне. Описал структуру для предметной области + методы. Разделение и конвертация в Utils для структуры.

---
+++ internal/models/route_test.go
@@ -0,0 +1,42 @@
+package route
+
+import (
+	"fmt"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestNew(t *testing.T) {
+	// sample new
+	resp, err := New(1, "test route name", 5.5)
+	expected := Route{
+		ID:        resp.ID,
+		UserID:    1,
+		RouteName: "test route name",
+		Length:    5.5,
+	}
+	assert.Assert(t, err == nil)
+	assert.Assert(t, resp == expected)
+
+	// negative lenght
+	resp, err = New(1, "test route name", -5.5)
+	assert.Assert(t, err.Error() == "route lenght must be greater than zero")
+	assert.Assert(t, resp == Route{})
+}
+
+func TestRouteTime(t *testing.T) {
+	// sample routeTime
+	rt := Route{1, 1, "test route name", 5}
+	resp, err := rt.RouteTime(rt, 2)
+	assert.Assert(t, err == nil)
+	assert.Assert(t, resp == 2.5)
+
+	// negative speed
+	rt = Route{1, 1, "test route name", 5}
+	resp, err = rt.RouteTime(rt, -2)
+	fmt.Println("resp", resp)
+	fmt.Println("err", err)
+	assert.Assert(t, err.Error() == "speed must be greater than zero")
+	assert.Assert(t, resp == 0)
+}
--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	route "ova_route_api/internal/models"
 	"reflect"
 	"testing"
 
@@ -9,7 +10,6 @@ import (
 )
 
 func TestSplitSlice(t *testing.T) {
-
 	resp, err := SplitSlice([]string{}, 5)
 	assert.Assert(t, err == nil)
 	assert.Assert(t, fmt.Sprint(resp) == "[]")
@@ -24,7 +24,6 @@ func TestSplitSlice(t *testing.T) {
 }
 
 func TestReverseKey(t *testing.T) {
-
 	resp, err := ReverseKey(map[int]string{})
 	assert.Assert(t, err == nil)
 	assert.Assert(t, reflect.DeepEqual(resp, map[string]int{}))
@@ -39,7 +38,6 @@ func TestReverseKey(t *testing.T) {
 }
 
 func TestFilterSlice(t *testing.T) {
-
 	arr := []int{}
 	resp, err := FilterSlice(arr, Ints)
 	assert.Assert(t, fmt.Sprint(resp) == "[]")
@@ -53,5 +51,107 @@ func TestFilterSlice(t *testing.T) {
 	arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	resp, err = FilterSlice(arr, Ints)
 	assert.Assert(t, fmt.Sprint(resp) == "[1 3 5 7]")
+	assert.Assert(t, err == nil)
+}
+
+func TestSplitToBulks(t *testing.T) {
+	// empty routes
+	routes := []route.Route{}
+	resp, err := SplitToBulks(routes, 2)
+	assert.Assert(t, fmt.Sprint(resp) == "[]")
+	assert.Assert(t, err == nil)
+
+	// sample routes
+	routes = []route.Route{
+		{
+			ID:        1,
+			UserID:    1,
+			RouteName: "Tets route",
+			Length:    10.5,
+		},
+		{
+			ID:        2,
+			UserID:    1,
+			RouteName: "Tets route 2",
+			Length:    6,
+		},
+		{
+			ID:        3,
+			UserID:    2,
+			RouteName: "Tets route 3",
+			Length:    13,
+		},
+	}
+	resp, err = SplitToBulks(routes, 2)
+	assert.Assert(t, fmt.Sprint(resp) == "[[{1 1 Tets route 10.5} {2 1 Tets route 2 6}] [{3 2 Tets route 3 13}]]")
+	assert.Assert(t, err == nil)
+
+	// change base routes
+	resp, err = SplitToBulks(routes, 2)
+
+	//nolint
+	routes = []route.Route{
+		{
+			ID:        4,
+			UserID:    3,
+			RouteName: "Tets route 4",
+			Length:    11,
+		},
+	}
+	assert.Assert(t, fmt.Sprint(resp) != "[[{4 3 Tets route 4 11}]]")
+	assert.Assert(t, err == nil)
+}
+
+func TestConvertToView(t *testing.T) {
+	// empty routes
+	routes := []route.Route{}
+	resp, err := ConvertToView(routes)
+	assert.Assert(t, resp == nil)
+	assert.Assert(t, err.Error() == "empty routes list")
+
+	// sample routes
+	routes = []route.Route{
+		{
+			ID:        1,
+			UserID:    1,
+			RouteName: "Tets route",
+			Length:    10.5,
+		},
+		{
+			ID:        2,
+			UserID:    1,
+			RouteName: "Tets route 2",
+			Length:    6,
+		},
+		{
+			ID:        3,
+			UserID:    2,
+			RouteName: "Tets route 3",
+			Length:    13,
+		},
+	}
+
+	expected := map[uint64]route.Route{
+		0: {
+			ID:        1,
+			UserID:    1,
+			RouteName: "Tets route",
+			Length:    10.5,
+		},
+		1: {
+			ID:        2,
+			UserID:    1,
+			RouteName: "Tets route 2",
+			Length:    6,
+		},
+		2: {
+			ID:        3,
+			UserID:    2,
+			RouteName: "Tets route 3",
+			Length:    13,
+		},
+	}
+	resp, err = ConvertToView(routes)
+	assert.DeepEqual(t, resp, expected)
 	assert.Assert(t, err == nil)
 }
